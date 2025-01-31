30d1a2fdfaa14c66d0363e82e03c7da34607e75c
Balestrino <salvatore.balestrino@gmail.com>
2025-01-31 07:58:54 +0000

    refactor: update Docker Compose configuration and add error handling in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
 import {
   MessagePattern,
   EventPattern,
   Ctx,
   NatsContext,
+  RpcException,
 } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
@@ -22,6 +23,15 @@ export class AppController {
     const headers = context.getHeaders();
     const subject = context.getSubject();
     return { message: 'OK', headers, subject };
+  }
+
+  @MessagePattern('user.error')
+  errorTrigger(@Ctx() context: NatsContext) {
+    console.log('errorTrigger');
+    throw new RpcException({
+      status: 'error',
+      message: 'This should be the error message!',
+    });
   }
 
   @EventPattern('user.healthcheck-event')
